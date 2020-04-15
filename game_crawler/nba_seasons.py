dbdd6be244f50d74436f18709fb2d2222a205beb
Anton1o-I <a.iniguez21@gmail.com>
2020-04-15 13:39:50 -0700

    updated game_links name, added start of nba_seasons dataclass for scraping

---
+++ game_crawler/nba_seasons.py
@@ -0,0 +1,14 @@
+import dataclasses
+from datetime import datetime
+
+@dataclass
+class Seasons:
+    season_info = {
+        "18-19": {
+            "regular_season_start": datetime(),
+            "regular_season_end": datetime(),
+            "post_season_start": datetime(),
+            "post_season_end"
+        }
+    }
+
