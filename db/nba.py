0d4722507502744a20cd9994fef075ba38e1d6d3
Anton1o-I <a.iniguez21@gmail.com>
2020-05-19 10:49:44 -0700

    Changed mappers from spider objects to db objects

--- db/nba.py
+++ db/nba.py
@@ -142,3 +142,112 @@ class nbaDB:
 
     def add_record(self, record):
         pass
+
+    def map_to_db(self, item):
+        game_data = item.get("game")
+        player_data = item.get("player_stats")
+        team_data = item.get("team_stats")
+        game = Game(
+            id=game_data.get("game_id", ""),
+            date=game_data.get("date", ""),
+            home_wins=game_data.get("home_record", {}).get("wins", ""),
+            home_losses=game_data.get("home_record", {}).get("losses", ""),
+            home_home_wins=game_data.get("home_home_record", {}).get("wins", ""),
+            home_home_losses=game_data.get("home_home_record", {}).get("losses", ""),
+            away_wins=game_data.get("away_record", {}).get("wins", ""),
+            away_losses=game_data.get("away_record", {}).get("losses", ""),
+            away_home_wins=game_data.get("away_away_record", {}).get("wins", ""),
+            away_home_losses=game_data.get("away_away_record", {}).get("losses", ""),
+            over_under=game_data.get("line", {}).get("ou", ""),
+            favorite=game_data.get("line", {}).get("favorite", ""),
+            spread=game_data.get("line", {}).get("spread", ""),
+        )
+
+        game.player_stats = self.map_player_stats(
+            player_data,
+            team_data.get("home_stats", {}).get("team", {}).get("abbreviation", ""),
+            team_data.get("away_stats", {}).get("team", {}).get("abbreviation", ""),
+        )
+        game.team_stats = self.map_team_stats(team_data)
+
+        return game
+
+    @staticmethod
+    def map_player_stats(player_data, home_pk, away_pk):
+        player_db_list = []
+        for k in player_data.keys():
+            for ps in player_data[k]:
+                stat = PlayerStat(
+                    player_id=ps.get("player", "")["id"],
+                    game_id=ps.get("game_id", ""),
+                    team_id=home_pk,
+                    minutes=ps.get("min", ""),
+                    points=ps.get("pts", ""),
+                    drebs=ps.get("drebs", ""),
+                    orebs=ps.get("orebs", ""),
+                    rebounds=ps.get("reb", ""),
+                    assists=ps.get("ast", ""),
+                    turnovers=ps.get("to", ""),
+                    fgm=ps.get("fgm", ""),
+                    fga=ps.get("fga", ""),
+                    fg_per=ps.get("fg_per", ""),
+                    ftm=ps.get("ftm", ""),
+                    fta=ps.get("fta", ""),
+                    ft_per=ps.get("ft_per", ""),
+                    x3pm=ps.get("x3pm", ""),
+                    x3pa=ps.get("x3pa", ""),
+                    x3p_per=ps.get("x3p_per", ""),
+                    steals=ps.get("stl", ""),
+                    fouls=ps.get("pf", ""),
+                    plus_minus=ps.get("plusminus", ""),
+                    player=Player(
+                        id=ps.get("player", {}).get("player_id", ""),
+                        first_name=ps.get("player", {}).get("first_name", ""),
+                        last_name=ps.get("player", {}).get("last_name", ""),
+                        pos=ps.get("player", {}).get("position", ""),
+                    ),
+                )
+                player_db_list.append(stat)
+        return player_db_list
+
+    @staticmethod
+    def map_team_stats(team_data):
+        team_db_list = []
+        for k in team_data.keys():
+            team = team_data[k]
+            team_stat = TeamStat(
+                game_id=team.get("game_id", ""),
+                team_id=team.get("team", {}).get("abbreviation", ""),
+                home=True if (k == "home") else False,
+                fgm=team.get("fgm", ""),
+                fga=team.get("fga", ""),
+                fg_per=team.get("fg_per", ""),
+                x3pm=team.get("x3pm", ""),
+                x3pa=team.get("x3pa", ""),
+                x3p_per=team.get("x3p_per", ""),
+                ftm=team.get("ftm", ""),
+                fta=team.get("fta", ""),
+                ft_per=team.get("ft_per", ""),
+                oreb=team.get("oreb", ""),
+                dreb=team.get("dreb", ""),
+                reb=team.get("reb", ""),
+                ast=team.get("ast", ""),
+                stl=team.get("stl", ""),
+                blk=team.get("blk", ""),
+                to=team.get("to", ""),
+                pts_off_to=team.get("pts_off_to", ""),
+                fast_break_pts=team.get("fast_break_pts", ""),
+                points_in_paint=team.get("points_in_paint", ""),
+                pf=team.get("pf", ""),
+                technical=team.get("technical", ""),
+                flagrant=team.get("flagrant", ""),
+                largest_lead=team.get("largest_lead", ""),
+                pts=team.get("pts", ""),
+                team=Team(
+                    location=team.get("team", {}).get("long_name", ""),
+                    name=team.get("team", {}).get("name", ""),
+                    abbr=team.get("team", {}).get("abbreviation", ""),
+                ),
+            )
+            team_db_list.append(team_stat)
+        return team_db_list
